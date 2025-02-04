a0e9c4adecbb5d3feb304f604f0ae2d7452d3f36
Vijayarahavan <vijayarahavan.k@purpleslate.in>
2025-02-04 18:20:58 +0530

    feat: iOS Native Module Integration

---
+++ ios/NativeLocalStorage/RCTNativeLocalStorage.h
@@ -0,0 +1,17 @@
+//
+//  RCTNativeLocalStorage.h
+//  TestNativeModules
+//
+//  Created by Purpleslate 14Mbp on 04/02/25.
+//
+
+#import <Foundation/Foundation.h>
+#import <NativeLocalStorageSpec/NativeLocalStorageSpec.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTNativeLocalStorage : NSObject <NativeLocalStorageSpec>
+
+@end
+
+NS_ASSUME_NONNULL_END
